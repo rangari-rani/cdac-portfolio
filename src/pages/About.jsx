a2679a017e11db79896fadf687e183bda980ec01
Rani Rangari <ranirangari2018@gmail.com>
2025-09-23 17:49:52 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
 
 
       <div className="py-10 flex flex-col">
-        <h3 className="subhead-text">My Skills (at the time)</h3>
+        <h3 className="subhead-text">My Skills</h3>
         <div className="mt-16 flex flex-wrap gap-12">
           {skills.map((skill) => (
             <div className="block-container w-20 h-20" key={skill.name}>
